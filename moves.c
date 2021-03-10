bf24a2ff44ff538a2171d4542baab48e106ad1e1
Pinkman Lurlene <plurlene@er-e4.kzn.21-school.ru>
2021-03-10 17:52:47 +0300

    djsdafhsfsl;l

--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: plurlene <plurlene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 16:36:49 by plurlene          #+#    #+#             */
-/*   Updated: 2021/03/10 17:02:33 by plurlene         ###   ########.fr       */
+/*   Updated: 2021/03/10 17:40:46 by plurlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,6 +108,6 @@ int			key_hook(int key_code, void *param)
 	mlx_do_sync(vars->mlx);
 	put_image(vars);
 	mlx_put_image_to_window(vars->mlx, vars->mlx_window, vars->img.img, 0, 0);
-	printf("x: %f y: %f \n", vars->player.x, vars->player.y);
+	printf("key_code: %d\n", key_code);
 	return (1);
 }
