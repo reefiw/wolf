3f9b1d6d9b580ad0babee3568c62e34c54a45999
Pinkman Lurlene <plurlene@er-e2.kzn.21-school.ru>
2021-03-12 17:58:23 +0300

    parser now in norm

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: plurlene <plurlene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 15:41:49 by plurlene          #+#    #+#             */
-/*   Updated: 2021/03/11 19:56:35 by plurlene         ###   ########.fr       */
+/*   Updated: 2021/03/12 17:57:49 by plurlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,10 @@ static int scip_digits(char *str, int flag, int bias)
 	return (i + bias);
 }
 
-static void check_error(int bool, char *err, char ***temp)
+static void check_error(int bool, char *err)
 {
 	if (bool)
-		error_handler_clear(err, *temp);
+		error_handler(err);
 }
 
 void resolution_parser(char ***temp, char *str, t_vars *vars)
@@ -55,23 +55,23 @@ void resolution_parser(char ***temp, char *str, t_vars *vars)
 
 	mlx_get_screen_size(vars->mlx, &mw, &mh);
 	res_str = ft_strtrim(str, " ");
-	check_error(res_str[0] != 'R', "invalid resolution\n", temp);
+	check_error(res_str[0] != 'R', "invalid resolution\n");
 	i = scip_whitespaces(&res_str[1], 1);
-	check_error(!ft_isdigit(res_str[i]), "invalid resolution\n", temp);
+	check_error(!ft_isdigit(res_str[i]), "invalid resolution\n");
 	vars->screen.width = 0;
 	vars->screen.width = ft_atoi(&res_str[i]);
 	vars->screen.width = vars->screen.width > mw ? mw : vars->screen.width;
 	while (ft_isdigit(res_str[i]))
 		i++;
 	i = scip_whitespaces(&res_str[i], i);
-	check_error(!ft_isdigit(res_str[i]), "invalid resolution\n", temp);
+	check_error(!ft_isdigit(res_str[i]), "invalid resolution\n");
 	vars->screen.height = 0;
 	vars->screen.height = ft_atoi(&res_str[i]);
 	vars->screen.height = vars->screen.height > mh ? mh : vars->screen.height;
-	check_error(!vars->screen.height || !vars->screen.width, "invalid resolution\n", temp);
+	check_error(!vars->screen.height || !vars->screen.width, "invalid resolution\n");
 	while (ft_isdigit(res_str[i]))
 		i++;
-	check_error(res_str[i] != '\0', "invalid resolution\n", temp);
+	check_error(res_str[i] != '\0', "invalid resolution\n");
 	free(res_str);
 }
 
@@ -138,7 +138,29 @@ static int ft_char_num(char *str, int c)
 	return (num);
 }
 
-static void check_color_str(char ***temp, char *str) // 40 строк
+static void check_color_space_format(int i, char **colors, char *temp_str, char *str)
+{
+	if (i != 3 && i != 0)
+		error_handler("invalid color\n");
+	if (!i)
+	{
+		colors = ft_split(str, ' ');
+		i = 0;
+		while (colors[i])
+		{
+			temp_str = ft_strtrim(colors[i], " ");
+			if (!ft_isnum(temp_str))
+				error_handler("invalid color\n");
+			i++;
+			free(temp_str);
+		}
+		clear_str_arr(colors);
+		if (i != 3 && i != 0)
+			error_handler("invalid color\n");
+	}
+}
+
+static void check_color_str(char ***temp, char *str)
 {
 	char **colors;
 	char *temp_str;
@@ -148,38 +170,16 @@ static void check_color_str(char ***temp, char *str) // 40 строк
 		error_handler_clear("invalid color\n", *temp);
 	colors = ft_split(str, ',');
 	i = 0;
-	while (colors[i])
+	while (colors[i] && colors[1])
 	{
 		temp_str = ft_strtrim(colors[i], " ");
 		if (!ft_isnum(temp_str))
-		{
-			free(temp_str);
-			clear_str_arr(colors);
-			error_handler_clear("invalid color\n", *temp);
-		}
+			error_handler("invalid color\n");
 		free(temp_str);
 		i++;
 	}
 	clear_str_arr(colors);
-	if (i != 3 && i != 0)
-		error_handler_clear("invalid color\n", *temp);
-	if (!i)
-	{
-		colors = ft_split(str, ' ');
-		i = 0;
-		while (colors[i])
-		{
-			i++;
-			if (!ft_isnum(temp_str))
-			{
-				clear_str_arr(colors);
-				error_handler_clear("invalid color\n", *temp);
-			}
-		}
-		clear_str_arr(colors);
-		if (i != 3 && i != 0)
-			error_handler_clear("invalid color\n", *temp);
-	}
+	check_color_space_format(i, colors, temp_str, str);
 }
 
 static void color_parser(char ***temp, char *str, t_vars *vars, int *color)
@@ -194,19 +194,19 @@ static void color_parser(char ***temp, char *str, t_vars *vars, int *color)
 	num = 0;
 	if (ft_isdigit(res_str[i]))
 		num = ft_atoi(&res_str[i]);
-	check_error(num > 255, "invalid color\n", temp);
+	check_error(num > 255, "invalid color\n");
 	*color = num << 16;
 	i = scip_digits(&res_str[i], 1, i);
 	i = scip_digits(&res_str[i], 0, i);
 	if (ft_isdigit(res_str[i]))
 		num = ft_atoi(&res_str[i]);
-	check_error(num > 255, "invalid color\n", temp);
+	check_error(num > 255, "invalid color\n");
 	*color = *color | num << 8;
 	i = scip_digits(&res_str[i], 1, i);
 	i = scip_digits(&res_str[i], 0, i);
 	if (ft_isdigit(res_str[i]))
 		num = ft_atoi(&res_str[i]);
-	check_error(num > 255, "invalid color\n", temp);
+	check_error(num > 255, "invalid color\n");
 	*color = *color | num;
 	free(res_str);
 }
@@ -274,7 +274,30 @@ static char **get_map(int fd, char *first_line)
 	return (result_arr);
 }
 
-static void init_sprites(t_vars *vars) // 31 строка
+static void put_sprites_in_array(t_vars *vars)
+{
+	int i;
+	int j;
+	int sprite_num;
+
+	i = -1;
+	sprite_num = 0;
+	while (vars->map->data[++i])
+	{
+		j = -1;
+		while (vars->map->data[i][++j])
+			if (vars->map->data[i][j] == '2')
+			{
+				vars->sprites[sprite_num] = (t_sprite *)malloc(sizeof(t_sprite));
+				vars->sprites[sprite_num]->x = j + 0.5;
+				vars->sprites[sprite_num]->y = i + 0.5;
+				vars->sprites[sprite_num]->len = 0;
+				sprite_num++;
+			}
+	}
+}
+
+static void init_sprites(t_vars *vars)
 {
 	int i;
 	int j;
@@ -292,21 +315,7 @@ static void init_sprites(t_vars *vars) // 31 строка
 	vars->sprites = (t_sprite **)malloc(sizeof(t_sprite *) * (num_sprites + 1));
 	vars->sprites[num_sprites] = NULL;
 	vars->num_sprites = num_sprites;
-	i = -1;
-	num_sprites = 0;
-	while (vars->map->data[++i])
-	{
-		j = -1;
-		while (vars->map->data[i][++j])
-			if (vars->map->data[i][j] == '2')
-			{
-				vars->sprites[num_sprites] = (t_sprite *)malloc(sizeof(t_sprite));
-				vars->sprites[num_sprites]->x = j + 0.5;
-				vars->sprites[num_sprites]->y = i + 0.5;
-				vars->sprites[num_sprites]->len = 0;
-				num_sprites++;
-			}
-	}
+	put_sprites_in_array(vars);
 }
 
 static void player_case(char s, t_player *player)
@@ -364,16 +373,14 @@ static void parse_player(t_vars *vars, char ***temp)
 		error_handler_clear("invalid map\n", *temp);
 }
 
-static char **get_head(int fd, t_vars *vars) //27 строк
+static char **get_head(int fd, t_vars *vars)
 {
 	char	*buf;
 	char	*line;
 	char	**temp;
 	int		k;
 
-	line = NULL;
-	buf = (char *)malloc(1);
-	buf[0] = '\0';
+	buf = (char *)calloc(1, 1);
 	while (get_next_line(fd, &line))
 	{
 		k = scip_whitespaces(line, 0);
@@ -424,38 +431,29 @@ static void check_map(char ***temp, t_vars *vars)
 	}
 }
 
-void main_parser(char *path, t_vars *vars) //32 строки
+void main_parser(char *path, t_vars *vars)
 {
 	int		fd;
 	int		k;
 	char	**temp;
 
-	if (ft_strncmp(&path[ft_strlen(path) - 4], ".cub", 4))
-		error_handler("invalid extension\n");
-	if	((fd = open(path, O_RDONLY)) < 1)
-		error_handler("can't open file\n");
+	check_error(ft_strncmp(&path[ft_strlen(path) - 4], ".cub", 4),\
+	"invalid extension\n");
+	check_error((fd = open(path, O_RDONLY)) < 1, "can't open file\n");
 	vars->map = (t_map *)malloc(sizeof(t_map));
-	temp = get_head(fd, vars); // <- 1 утечка тут
+	temp = get_head(fd, vars);
 	init_sprites(vars);
 	parse_player(vars, &temp);
 	close(fd);
-	k = 0;
-	while (vars->map->data[k])
-		k++;
+	k = -1;
+	while (vars->map->data[++k])
+		vars->map->height = k + 1;
 	check_map(&temp, vars);
-	vars->map->height = k;
 	fd = -1;
 	k = 0;
-	while (temp[++fd])
-	{
-		k += parser_switch(&temp, temp[fd], vars);
-		printf("%d) k = %d %s\n", fd, k, temp[fd]);
-		if (k > 8)
-			error_handler("invalid file1\n");
-	}
-//	free(temp);
+	while (temp[++fd] && (k += parser_switch(&temp, temp[fd], vars)))
+		check_error(k > 8, "invalid file\n");
 	clear_str_arr(temp);
 	vars->z_buffer = (double *)malloc(sizeof(double) * vars->screen.width);
-	if (fd != 8 || k != 8)
-		error_handler("invalid file2\n");
+	check_error(fd != 8 || k != 8, "invalid file\n");
 }
